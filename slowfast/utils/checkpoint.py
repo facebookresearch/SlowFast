972cb6b03b5d66c82e57411aff61dafcf6f8acab
Tran Le <quytranle@fb.com>
2020-08-04 13:40:08 -0700

    Fix load test checkpoint
    Summary: Loading test checkpoint was using checkpoint type from train. This diff fixes that.
    
    Reviewed By: bxiong1202, lyttonhao
    
    Differential Revision: D22911520
    
    fbshipit-source-id: 6c92eb94649f5ef194ad49307e7df79b02f1343a

--- slowfast/utils/checkpoint.py
+++ slowfast/utils/checkpoint.py
@@ -404,7 +404,7 @@ def load_test_checkpoint(cfg, model):
             cfg.NUM_GPUS > 1,
             None,
             inflation=False,
-            convert_from_caffe2=cfg.TRAIN.CHECKPOINT_TYPE == "caffe2",
+            convert_from_caffe2=cfg.TEST.CHECKPOINT_TYPE == "caffe2",
         )
     elif has_checkpoint(cfg.OUTPUT_DIR):
         last_checkpoint = get_last_checkpoint(cfg.OUTPUT_DIR)
