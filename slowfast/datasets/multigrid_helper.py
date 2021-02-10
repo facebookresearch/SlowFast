fcdc82927dd44b0ae29c723e3dfc0ca3885ff9ff
ShoufaChen <shoufahappy@gmail.com>
2021-02-10 09:16:52 +0800

    keep existing for pytorch1.7 and earlier

--- slowfast/datasets/multigrid_helper.py
+++ slowfast/datasets/multigrid_helper.py
@@ -4,7 +4,16 @@
 """Helper functions for multigrid training."""
 
 import numpy as np
+import torch
 from torch.utils.data.sampler import Sampler
+
+TORCH_MAJOR = int(torch.__version__.split('.')[0])
+TORCH_MINOR = int(torch.__version__.split('.')[1])
+
+if TORCH_MAJOR >= 1 and TORCH_MINOR >= 8:
+    _int_classes = int
+else:
+    from torch._six import int_classes as _int_classes
 
 
 class ShortCycleBatchSampler(Sampler):
@@ -21,7 +30,7 @@ class ShortCycleBatchSampler(Sampler):
                 "torch.utils.data.Sampler, but got sampler={}".format(sampler)
             )
         if (
-            not isinstance(batch_size, int)
+            not isinstance(batch_size, _int_classes)
             or isinstance(batch_size, bool)
             or batch_size <= 0
         ):
