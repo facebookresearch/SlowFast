3dbf87bd761d9d5dbc0579ea5fe29db02ab024ba
Chay Ryali <chayryali@fb.com>
2022-05-31 20:05:52 -0700

    Move PySF operators to PTV
    Summary: Using swish from ptv in pysf, reducing redundancy
    
    Reviewed By: feichtenhofer, lyttonhao
    
    Differential Revision: D36367989
    
    fbshipit-source-id: 92b8c46108fb74a4dbc0be1e7eeea637f2e816d7

--- slowfast/models/operators.py
+++ slowfast/models/operators.py
@@ -3,34 +3,8 @@
 
 """Custom operators."""
 
-import torch
 import torch.nn as nn
-
-
-class Swish(nn.Module):
-    """Swish activation function: x * sigmoid(x)."""
-
-    def __init__(self):
-        super(Swish, self).__init__()
-
-    def forward(self, x):
-        return SwishEfficient.apply(x)
-
-
-class SwishEfficient(torch.autograd.Function):
-    """Swish activation function: x * sigmoid(x)."""
-
-    @staticmethod
-    def forward(ctx, x):
-        result = x * torch.sigmoid(x)
-        ctx.save_for_backward(x)
-        return result
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        x = ctx.saved_variables[0]
-        sigmoid_x = torch.sigmoid(x)
-        return grad_output * (sigmoid_x * (1 + x * (1 - sigmoid_x)))
+from pytorchvideo.layers.swish import Swish
 
 
 class SE(nn.Module):
