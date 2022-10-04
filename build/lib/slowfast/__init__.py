3ea834aabc9de4432bbfde9f74793927e02c2796
Uitaek Hong <50959890+Unshapendwarf@users.noreply.github.com>
2022-10-04 13:56:51 +0900

    Delete build/lib/slowfast directory

--- build/lib/slowfast/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-
-from slowfast.utils.env import setup_environment
-
-setup_environment()
--- build/lib/slowfast/models/custom_video_model_builder.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-
-
-"""A More Flexible Video models."""
