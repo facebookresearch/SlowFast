5e21a503d41a6711714be17537cd96a622862c1f
Uitaek Hong <dmlxor8422@gmail.com>
2022-10-28 12:40:28 +0900

    run.sh changed

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 # /bin/bash
-
 WORK_SPACE=$HOME/slowfast/
 cd $WORK_SPACE
-# cd "$HOME/dataset/"
 echo $PWD
+/home/hong/miniconda3/envs/sf/bin/python tools/run_net.py --cfg configs/contrastive_ssl/custom_BYOL_SlowR50_8x8.yaml
 
-
-python tools/run_net.py --cfg configs/contrastive_ssl/custom_BYOL_SlowR50_8x8.yaml
-
-# check another metrics for this such as epochs, num_workers, batch_size, and learning_rate.
