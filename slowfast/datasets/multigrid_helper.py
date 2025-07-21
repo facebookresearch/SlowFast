a1708acb9953d854ba397fec569dac8eb3727315
Anudeep Tippabathuni <120025658+Anudeep007-hub@users.noreply.github.com>
2025-07-21 19:09:09 +0530

    Fix: Enable decoding custom .mp4 videos in Kinetics-style datasets

--- slowfast/datasets/multigrid_helper.py
+++ slowfast/datasets/multigrid_helper.py
@@ -7,13 +7,11 @@ import numpy as np
 import torch
 from torch.utils.data.sampler import Sampler
 
-TORCH_MAJOR = int(torch.__version__.split(".")[0])
-TORCH_MINOR = int(torch.__version__.split(".")[1])
-
-if TORCH_MAJOR >= 1 and TORCH_MINOR >= 8:
-    _int_classes = int
-else:
+# Modified this import 
+try:
     from torch._six import int_classes as _int_classes
+except ImportError:
+    _int_classes = (int,)
 
 
 class ShortCycleBatchSampler(Sampler):
